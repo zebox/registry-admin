613ab0a83fff02e5bcfdaee0f7fe599c5ffff8c7
zebox <yurok712@mail.ru>
2022-07-22 20:34:05 +0300

    added test for push event handler

--- app/store/service/repository_events_test.go
+++ app/store/service/repository_events_test.go
@@ -4,18 +4,78 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/base64"
+	"github.com/docker/distribution/notifications"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/zebox/registry-admin/app/store"
 	"github.com/zebox/registry-admin/app/store/engine"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestDataService_RepositoryEventsProcessing(t *testing.T) {
 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	ds := DataService{
+		Repository: prepareEngineMock(),
+	}
+	testEnvelope := notifications.Envelope{
+		Events: []notifications.Event{
+			{
+				ID:        "320678d8-ca14-430f-8bb6-4ca139cd83f7",
+				Timestamp: time.Now(),
+				Action:    notifications.EventActionPush,
+				Request: notifications.RequestRecord{
+					ID:        "6df24a34-0959-4923-81ca-14f09767db19",
+					Addr:      "192.168.64.11:42961",
+					Host:      "192.168.100.227:5000",
+					Method:    "GET",
+					UserAgent: "curl/7.38.0",
+				},
+				Actor: notifications.ActorRecord{},
+				Source: notifications.SourceRecord{
+					Addr:       "xtal.local:5000",
+					InstanceID: "a53db899-3b4b-4a62-a067-8dd013beaca4",
+				},
+			},
+		},
+	}
+
+	testEnvelope.Events[0].Target.Repository = "test/repo_1"
+	testEnvelope.Events[0].Target.Tag = "1.1.0"
+	testEnvelope.Events[0].Target.MediaType = "application/vnd.docker.distribution.manifest.v2+json"
+	testEnvelope.Events[0].Target.Digest = "sha256:fea8895f450959fa676bcc1df0611ea93823a735a01205fd8622846041d0c7cf"
+	testEnvelope.Events[0].Target.Length = 708
+	testEnvelope.Events[0].Target.Size = 708
+	testEnvelope.Events[0].Target.URL = "http://192.168.100.227:5000/v2/hello-world/manifests/sha256:fea8895f450959fa676bcc1df0611ea93823a735a01205fd8622846041d0c7cf"
+
+	err := ds.RepositoryEventsProcessing(ctx, testEnvelope)
+	assert.NoError(t, err)
+
+	// test with not exist repository
+	testEnvelope.Events[0].Target.Repository = "test/repo_3"
+	testEnvelope.Events[0].Target.Tag = "1.1.0"
+	err = ds.RepositoryEventsProcessing(ctx, testEnvelope)
+	assert.NoError(t, err)
+
+	// test with multiple values
+	testEnvelope.Events[0].Target.Repository = "test/repo_"
+	testEnvelope.Events[0].Target.Tag = "1."
+	err = ds.RepositoryEventsProcessing(ctx, testEnvelope)
+	assert.Error(t, err)
+
+	// test with filter error
+	testEnvelope.Events[0].Target.Repository = ""
+	testEnvelope.Events[0].Target.Tag = ""
+	err = ds.RepositoryEventsProcessing(ctx, testEnvelope)
+	assert.Error(t, err)
 }
 
 func prepareEngineMock() *engine.InterfaceMock {
+
 	testRepositoriesEntries := []store.RegistryEntry{
 		{
 			RepositoryName: "test/repo_1",
@@ -63,6 +123,34 @@ func prepareEngineMock() *engine.InterfaceMock {
 				}
 			}
 			return errors.New("entry not found")
+		},
+
+		FindRepositoriesFunc: func(ctx context.Context, filter engine.QueryFilter) (result engine.ListResponse, err error) {
+			if _, ok := filter.Filters["repository_name"]; !ok {
+				return result, errors.New("empty repository name not allowed")
+			}
+			searchRepo := filter.Filters["repository_name"].(string)
+
+			if _, ok := filter.Filters["tag"]; !ok {
+				return result, errors.New("empty repository name not allowed")
+			}
+			searchTag := filter.Filters["tag"].(string)
+
+			if searchRepo == "" || searchTag == "" {
+				return result, errors.New("empty repository name or tag not allowed")
+			}
+			var counter int64
+			for _, rep := range testRepositoriesEntries {
+
+				if strings.HasPrefix(rep.RepositoryName, searchRepo) && strings.HasPrefix(rep.Tag, searchTag) {
+					counter++
+					result = engine.ListResponse{
+						Total: counter,
+						Data:  append(result.Data, rep),
+					}
+				}
+			}
+			return result, nil
 		},
 	}
 }
