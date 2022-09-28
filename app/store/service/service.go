458b3d6b35826d1e7c0d2065b22a2b0ac1094d4a
zebox <yurok712@mail.ru>
2022-09-28 07:57:11 +0300

    implement save repos to storage at sync

--- app/store/service/service.go
+++ app/store/service/service.go
@@ -2,9 +2,13 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/mattn/go-sqlite3"
 	"github.com/pkg/errors"
 	"github.com/zebox/registry-admin/app/registry"
+	"github.com/zebox/registry-admin/app/store"
 	"github.com/zebox/registry-admin/app/store/engine"
+	"time"
 
 	log "github.com/go-pkgz/lgr"
 )
@@ -51,8 +55,9 @@ func (ds *DataService) SyncExistedRepositories(ctx context.Context) error {
 		for {
 			repos, err := ds.Registry.Catalog(context.Background(), n, lastRepo)
 
-			if err != nil && !errors.Is(err, registry.ErrNoMorePages) {
-				log.Printf("failed to syncing existed repositories: %v", err)
+			if err != nil && err != registry.ErrNoMorePages {
+				log.Printf("failed to fetch catalog list: %v", err)
+				log.Printf("[ERROR] sync operation aborted")
 				break
 			}
 
@@ -68,6 +73,7 @@ func (ds *DataService) SyncExistedRepositories(ctx context.Context) error {
 				tags, errTags := ds.Registry.ListingImageTags(ctx, repo, defaultPageSize, lastTag)
 				if errTags != nil && !errors.Is(errTags, registry.ErrNoMorePages) {
 					log.Printf("[ERROR] failed to repository tags at repository '%s': %v", repo, errTags)
+					log.Printf("[ERROR] sync operation aborted")
 					return
 				}
 
@@ -77,9 +83,34 @@ func (ds *DataService) SyncExistedRepositories(ctx context.Context) error {
 					log.Printf("[DEBUG] Tag name: %s\n", tag)
 					manifest, errManifest := ds.Registry.Manifest(ctx, repo, tag)
 					if errManifest != nil {
+						log.Printf("[ERROR] failed to fetch manifest from repo '%s' for tag '%s' err: %s", repo, tag, errManifest)
+						log.Printf("[ERROR] sync operation aborted")
 						return
 					}
 					log.Printf("[DEBUG] Manifest: %v\n", manifest)
+					rawManifestData, errMarshal := json.Marshal(&manifest)
+					if errMarshal != nil {
+						log.Printf("[ERROR] failed to marshal manifest data from repo '%s' for tag '%s' err: %s", repo, tag, errMarshal)
+					}
+
+					entry := &store.RegistryEntry{
+						RepositoryName: repo,
+						Tag:            tag,
+						Digest:         manifest.ContentDigest,
+						Size:           manifest.TotalSize,
+						Timestamp:      time.Now().Unix(),
+						Raw:            rawManifestData,
+					}
+					if errCreate := ds.Storage.CreateRepository(ctx, entry); err != nil {
+						if errCreate == sqlite3.ErrConstraintUnique {
+							log.Printf("[ERROR] failed to marshal manifest data from repo '%s' for tag '%s' err: %s", repo, tag, errCreate)
+							log.Printf("[ERROR] sync operation aborted")
+							return
+						}
+						log.Printf("[WARN] entry already exist and skipped for add: repo: '%s', tag: '%s'")
+						continue
+					}
+					log.Printf("[DEBUG] New entry added: repo: '%s', tag: '%s'", repo, tag)
 				}
 
 				if errors.Is(errTags, registry.ErrNoMorePages) {
@@ -90,6 +121,7 @@ func (ds *DataService) SyncExistedRepositories(ctx context.Context) error {
 				_, lastTag, err = registry.ParseUrlForNextLink(tags.NextLink)
 				if err != nil {
 					log.Printf("[ERROR] failed to parse next link: %v", err)
+					log.Printf("[ERROR] sync operation aborted")
 					break
 				}
 			}
