50efebd066e7d259ed4b4c43164e27dad773f424
zebox <yurok712@mail.ru>
2022-10-14 08:09:02 +0300

    refactor

--- frontend/src/groups/GroupList.tsx
+++ frontend/src/groups/GroupList.tsx
@@ -6,7 +6,7 @@ import {
     TextField,
     EditButton,
     DeleteButton,
-     
+
 } from 'react-admin';
 
 
@@ -15,10 +15,12 @@ const GroupList = () => (
         sort={{ field: 'name', order: 'ASC' }}
         perPage={25}
     >
-        <Datagrid bulkActionButtons={false} >
+        <Datagrid bulkActionButtons={false}
+
+         >
                 <TextField source="id"  />
                 <TextField source="name" />
-                <EditButton alignIcon="right"/>
+                <EditButton alignIcon="right" />
                 <DeleteButton alignIcon="right" />
         </Datagrid>
     </List>
