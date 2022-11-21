63b140f5339061943cae3e8f4abc50846b10d892
zebox <yurok712@mail.ru>
2022-11-21 07:58:50 +0300

    change menu items position

--- frontend/src/layout/Menu.tsx
+++ frontend/src/layout/Menu.tsx
@@ -4,7 +4,8 @@ import {
     MenuItemLink,
     MenuProps,
     useSidebarState,
-    usePermissions
+    usePermissions,
+    NullableBooleanInputClasses
 } from 'react-admin';
 
 import users from '../users';
@@ -12,62 +13,66 @@ import groups from '../groups';
 import access from '../access';
 import repository from '../registry';
 
-const Menu = ({ dense = false }: MenuProps) => {
+import {requirePermission} from '../helpers/Helpers';
 
-    const { permissions } = usePermissions();
+const Menu = ({dense = false}: MenuProps) => {
+
+    const {permissions} = usePermissions();
     const translate = useTranslate();
     const [open] = useSidebarState();
 
     return (<Box
-        sx={{
-            width: open ? 200 : 50,
-            marginTop: 1,
-            marginBottom: 1,
-            transition: theme =>
-                theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-        }}
-    >
-        {permissions && permissions.role==='admin' ? <>
-        <MenuItemLink
-            to="/users"
-            state={{ _scrollToTop: true }}
-            primaryText={translate(`resources.commands.users_name`, {
-                smart_count: 2,
-            })}
-            leftIcon={<users.icon />}
-            dense={dense}
-        />
-        <MenuItemLink
-            to="/groups"
-            state={{ _scrollToTop: true }}
-            primaryText={translate(`resources.commands.groups_name`, {
-                smart_count: 2,
-            })}
-            leftIcon={<groups.icon />}
-            dense={dense}
-        />
-        </>:null}
-        <MenuItemLink
-            to="/access"
-            state={{ _scrollToTop: true }}
-            primaryText={translate(`resources.commands.access_name`, {
-                smart_count: 2,
-            })}
-            leftIcon={<access.icon />}
-            dense={dense}
-        />
-        <MenuItemLink
-            to="/registry/catalog"
-            state={{ _scrollToTop: true }}
-            primaryText={translate(`resources.commands.repository_name`, {
-                smart_count: 2,
-            })}
-            leftIcon={<repository.icon />}
-            dense={dense}
-        />
+            sx={{
+                width: open ? 200 : 50,
+                marginTop: 1,
+                marginBottom: 1,
+                transition: theme =>
+                    theme.transitions.create('width', {
+                        easing: theme.transitions.easing.sharp,
+                        duration: theme.transitions.duration.leavingScreen,
+                    }),
+            }}
+        >
+            <MenuItemLink
+                to="/registry/catalog"
+                state={{_scrollToTop: true}}
+                primaryText={translate(`resources.commands.repository_name`, {
+                    smart_count: 2,
+                })}
+                leftIcon={<repository.icon/>}
+                dense={dense}
+            />
+            {requirePermission(permissions, 'admin') || requirePermission(permissions, 'manager') ?
+                <MenuItemLink
+                    to="/access"
+                    state={{_scrollToTop: true}}
+                    primaryText={translate(`resources.commands.access_name`, {
+                        smart_count: 2,
+                    })}
+                    leftIcon={<access.icon/>}
+                    dense={dense}
+                /> : null
+            }
+            {requirePermission(permissions, 'admin') ? <>
+                <MenuItemLink
+                    to="/users"
+                    state={{_scrollToTop: true}}
+                    primaryText={translate(`resources.commands.users_name`, {
+                        smart_count: 2,
+                    })}
+                    leftIcon={<users.icon/>}
+                    dense={dense}
+                />
+                <MenuItemLink
+                    to="/groups"
+                    state={{_scrollToTop: true}}
+                    primaryText={translate(`resources.commands.groups_name`, {
+                        smart_count: 2,
+                    })}
+                    leftIcon={<groups.icon/>}
+                    dense={dense}
+                />
+            </> : null}
     </Box>
 
 
