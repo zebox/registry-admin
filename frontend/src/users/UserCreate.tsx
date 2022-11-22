064a90a8ecceb7285994823673ead354798b26c9
zebox <yurok712@mail.ru>
2022-11-22 20:59:49 +0300

    added required for field

--- frontend/src/users/UserCreate.tsx
+++ frontend/src/users/UserCreate.tsx
@@ -8,32 +8,41 @@ import {
     SimpleForm,
     useTranslate,
     usePermissions,
-    NotFound
+    NotFound,
+    required
 } from 'react-admin';
 import {requirePermission} from '../helpers/Helpers';
 import {RoleList} from "./UsersList";
 
-export const UserCreate = () => {
+export const UserCreate = (props:any) => {
     const translate = useTranslate();
+    const {source, ...rest} = props;
     const {permissions} = usePermissions();
 
     return (requirePermission(permissions, 'admin') ?
             <Create title={translate('resources.users.add_title')}>
                 <SimpleForm>
-                    <TextInput label={translate('resources.users.fields.login')} source="login"/>
-                    <TextInput label={translate('resources.users.fields.name')} source="name" autoComplete='off'/>
+                    <TextInput label={translate('resources.users.fields.login')} source="login" {...rest} validate={required()}/>
+                    <TextInput label={translate('resources.users.fields.name')} source="name" autoComplete='off' {...rest} validate={required()}/>
                     <PasswordInput label={translate('resources.users.fields.password')} source="password"
-                                   autoComplete="new-password"/>
+                                   autoComplete="new-password" {...rest} validate={required()}/>
                     <ReferenceInput source="group" reference="groups">
-                        <SelectInput label={translate('resources.users.fields.group')} emptyValue={""} emptyText=''
-                                     optionText="name" optionValue="id"/>
+                        <SelectInput label={translate('resources.users.fields.group')} 
+                        emptyValue={""} 
+                        emptyText='' 
+                        defaultValue={1}
+                        optionText="name" optionValue="id"
+                        {...rest} validate={required()}
+                         />
                     </ReferenceInput>
                     <SelectInput
                         label={translate('resources.users.fields.role')}
                         source="role"
                         defaultValue={"user"}
                         emptyValue={""}
-                        choices={RoleList}/>
+                        choices={RoleList}
+                        {...rest} validate={required()}
+                        />
                     <BooleanInput label={translate('resources.users.fields.blocked')} source="blocked"/>
                     <TextInput label={translate('resources.users.fields.description')} source="description"
                                autoComplete='off' fullWidth/>
--- frontend/src/users/UserEdit.tsx
+++ frontend/src/users/UserEdit.tsx
@@ -9,37 +9,47 @@ import {
     SimpleForm,
     useTranslate,
     usePermissions,
-    NotFound
+    NotFound,
+    required
 } from 'react-admin';
-import {requirePermission} from "../helpers/Helpers";
-import {RoleList} from "./UsersList";
+import { requirePermission } from "../helpers/Helpers";
+import { RoleList } from "./UsersList";
 
-const UserEdit = () => {
+const UserEdit = (props: any) => {
     const translate = useTranslate();
-    const {permissions} = usePermissions();
+    const { source, ...rest } = props;
+    const { permissions } = usePermissions();
     return (requirePermission(permissions, 'admin') ?
-            <Edit title={translate('resources.users.edit_title')}>
-                <SimpleForm>
-                    <TextInput label="ID" source="id" disabled/>
-                    <TextInput label={translate('resources.users.fields.login')} source="login" disabled/>
-                    <TextInput label={translate('resources.users.fields.name')} source="name" autoComplete='off'/>
-                    <PasswordInput label={translate('resources.users.fields.password')} source="password"
-                                   autoComplete="new-password"/>
-                    <ReferenceInput source="group" reference="groups">
-                        <SelectInput label={translate('resources.users.fields.group')} emptyValue={''} emptyText=''
-                                     optionText="name" optionValue="id"/>
-                    </ReferenceInput>
-                    <SelectInput
-                        label={translate('resources.users.fields.role')}
-                        source="role"
-                        defaultValue={"user"}
-                        emptyValue={''}
-                        choices={RoleList}/>
-                    <BooleanInput label={translate('resources.users.fields.blocked')} source="blocked"/>
-                    <TextInput label={translate('resources.users.fields.description')} source="description"
-                               autoComplete='off' fullWidth/>
-                </SimpleForm>
-            </Edit> : <NotFound/>
+        <Edit title={translate('resources.users.edit_title')}>
+            <SimpleForm>
+                <TextInput label="ID" source="id" disabled />
+                <TextInput label={translate('resources.users.fields.login')} source="login" disabled />
+                <TextInput label={translate('resources.users.fields.name')} source="name" autoComplete='off' {...rest} validate={required()} />
+                <PasswordInput label={translate('resources.users.fields.password')}
+                    source="password"
+                    autoComplete="new-password"
+                    {...rest} validate={required()} />
+                <ReferenceInput source="group" reference="groups">
+                    <SelectInput label={translate('resources.users.fields.group')} 
+                        emptyValue={''} 
+                        emptyText=''
+                        optionText="name" 
+                        optionValue="id" 
+                        {...rest} validate={required()}
+                        />
+                </ReferenceInput>
+                <SelectInput
+                    label={translate('resources.users.fields.role')}
+                    source="role"
+                    defaultValue={"user"}
+                    emptyValue={''}
+                    choices={RoleList} 
+                    {...rest} validate={required()}/>
+                <BooleanInput label={translate('resources.users.fields.blocked')} source="blocked" />
+                <TextInput label={translate('resources.users.fields.description')} source="description"
+                    autoComplete='off' fullWidth />
+            </SimpleForm>
+        </Edit> : <NotFound />
     )
 };
 
