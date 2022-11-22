1ac8a6add006c268c1f8106a896eb05c7e9a114f
zebox <yurok712@mail.ru>
2022-11-22 08:31:48 +0300

    added permissions for access add and edit

--- frontend/src/access/AccessCreate.tsx
+++ frontend/src/access/AccessCreate.tsx
@@ -1,31 +1,48 @@
 
-import {required, AutocompleteInput, Create, TextInput, SimpleForm, ReferenceInput, BooleanInput, useTranslate } from 'react-admin';
-import { RepositoryAutocomplete } from '../components/RepositoryAutocompleteField';
+import {
+    required,
+    AutocompleteInput,
+    Create,
+    NotFound,
+    TextInput,
+    SimpleForm,
+    ReferenceInput,
+    BooleanInput,
+    useTranslate,
+    usePermissions
+} from 'react-admin';
+import {RepositoryAutocomplete} from '../components/RepositoryAutocompleteField';
 import RepositoryAction from "../components/RepositoryAction";
+import {requirePermission} from '../helpers/Helpers';
 
 export const AccessCreate = (props:any) => {
-    const { source, ...rest } = props;
+    const {source, ...rest} = props;
     const translate = useTranslate();
-  
-    return (
-        <Create title={translate('resources.accesses.add_title')} >
-            <SimpleForm>
-                <TextInput sx={{ width: "30%" }} label={translate('resources.accesses.fields.name')} source="name" validate={required()}/>
-                <ReferenceInput source="owner_id" reference="users" label={translate('resources.accesses.fields.owner_id')} >
-                    <AutocompleteInput sx={{ width: "30%" }} optionText="name" optionValue="id" label={translate('resources.accesses.fields.owner_id')}  validate={required()}/>
-                </ReferenceInput>
-                <RepositoryAutocomplete source="resource_name" {...rest} validate={required()} />            
-                <RepositoryAction source="action" validate={required()} defaultValue="pull"/>
-                <TextInput
-                    label={translate('resources.accesses.fields.resource_type')}
-                    source="type"
-                    defaultValue={"repository"}
-                    disabled
-                />
-                <BooleanInput label={translate('resources.accesses.fields.disabled')} source="disabled" />
-            </SimpleForm>
+    const {permissions} = usePermissions();
 
-        </Create>
+    return (requirePermission(permissions, 'admin') ?
+            <Create title={translate('resources.accesses.add_title')}>
+                <SimpleForm>
+                    <TextInput sx={{width: "30%"}} label={translate('resources.accesses.fields.name')} source="name"
+                               validate={required()}/>
+                    <ReferenceInput source="owner_id" reference="users"
+                                    label={translate('resources.accesses.fields.owner_id')}>
+                        <AutocompleteInput sx={{width: "30%"}} optionText="name" optionValue="id"
+                                           label={translate('resources.accesses.fields.owner_id')}
+                                           validate={required()}/>
+                    </ReferenceInput>
+                    <RepositoryAutocomplete source="resource_name" {...rest} validate={required()}/>
+                    <RepositoryAction source="action" validate={required()} defaultValue="pull"/>
+                    <TextInput
+                        label={translate('resources.accesses.fields.resource_type')}
+                        source="type"
+                        defaultValue={"repository"}
+                        disabled
+                    />
+                    <BooleanInput label={translate('resources.accesses.fields.disabled')} source="disabled"/>
+                </SimpleForm>
+
+            </Create> : <NotFound/>
     )
 };
 
--- frontend/src/access/AccessEdit.tsx
+++ frontend/src/access/AccessEdit.tsx
@@ -1,31 +1,46 @@
 
-import { AutocompleteInput, BooleanInput, Edit, TextInput, SimpleForm, ReferenceInput, useTranslate, required  } from 'react-admin';
-import { RepositoryAutocomplete } from "../components/RepositoryAutocompleteField";
+import {
+    AutocompleteInput,
+    BooleanInput,
+    Edit,
+    NotFound,
+    TextInput,
+    SimpleForm,
+    ReferenceInput,
+    useTranslate,
+    usePermissions,
+    required
+} from 'react-admin';
+import {RepositoryAutocomplete} from "../components/RepositoryAutocompleteField";
 import RepositoryAction from "../components/RepositoryAction";
+import {requirePermission} from '../helpers/Helpers';
 
 const AccessEdit = (props:any) => {
     const { source, ...rest } = props;
     const translate = useTranslate();
+    const {permissions} = usePermissions();
 
-    return (
-        <Edit title={translate('resources.groups.edit_title')}  >
-            <SimpleForm>
-                <TextInput sx={{ width: "30%" }} label={translate('resources.accesses.fields.name')} source="name" validate={required()} />
-                <ReferenceInput source="owner_id" reference="users">
-                    <AutocompleteInput sx={{ width: "30%" }} optionText="name" optionValue="id" label={translate('resources.accesses.fields.owner_id')} validate={required()}/>
-                </ReferenceInput>
-                <RepositoryAutocomplete source="resource_name"  validate={required()} {...rest} />
-                <RepositoryAction source="action"/>
-                <TextInput
-                    label={translate('resources.accesses.fields.resource_type')}
-                    source="type"
-                    defaultValue={"repository"}
-                    disabled
-                />
-                <BooleanInput label={translate('resources.accesses.fields.disabled')} source="disabled" />
-            </SimpleForm>
-
-        </Edit>
+    return (requirePermission(permissions, 'admin') ?
+            <Edit title={translate('resources.groups.edit_title')}>
+                <SimpleForm>
+                    <TextInput sx={{width: "30%"}} label={translate('resources.accesses.fields.name')} source="name"
+                               validate={required()}/>
+                    <ReferenceInput source="owner_id" reference="users">
+                        <AutocompleteInput sx={{width: "30%"}} optionText="name" optionValue="id"
+                                           label={translate('resources.accesses.fields.owner_id')}
+                                           validate={required()}/>
+                    </ReferenceInput>
+                    <RepositoryAutocomplete source="resource_name" validate={required()} {...rest} />
+                    <RepositoryAction source="action"/>
+                    <TextInput
+                        label={translate('resources.accesses.fields.resource_type')}
+                        source="type"
+                        defaultValue={"repository"}
+                        disabled
+                    />
+                    <BooleanInput label={translate('resources.accesses.fields.disabled')} source="disabled"/>
+                </SimpleForm>
+            </Edit> : <NotFound/>
     )
 };
 
