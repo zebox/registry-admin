d657c95c797523eab161bef47bf35b6a7b3c146d
zebox <yurok712@mail.ru>
2022-12-23 07:37:41 +0300

    added config examples

--- _examples/test_image_pusher.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {2..200}
-do
-   echo "pushed $i times"
-   docker image tag hello-world:latest 127.0.0.1:50554/test_1/hello-world:0.$i.0
-   docker image push 127.0.0.1:50554/test_1/hello-world:0.$i.0
-done
